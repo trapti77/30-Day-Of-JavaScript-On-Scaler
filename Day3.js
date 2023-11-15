435b0a9435a2e1591423bd9ecde2ba473ae23dfa
Trapti Patel <114275270+trapti77@users.noreply.github.com>
2023-11-15 20:30:16 +0530

    Day3

---
+++ Day3.js
@@ -0,0 +1,12 @@
+
+function checkrange(n1,n2) {
+    if(n1>=50&&n2<=99)
+    {
+        return true;
+    }
+    return false;
+}
+console.log(checkrange(40,80));
+console.log(checkrange(55,100));
+console.log(checkrange(50,30));
+console.log(checkrange(40,80));
